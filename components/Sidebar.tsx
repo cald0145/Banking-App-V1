2edd6802b0fc0f7e20ebec367d3d0f64fde86e9e
Jay Calderon <cald0145@algonquinlive.com>
2024-06-05 19:12:15 -0400

    Add visible sidebar routes and highlighted links

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -4,7 +4,6 @@ import { cn } from '@/lib/utils'
 import Image from 'next/image'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
-import React from 'react'
 
 const Sidebar = ({user}: SiderbarProps) => {
   const pathname = usePathname();
@@ -12,7 +11,7 @@ const Sidebar = ({user}: SiderbarProps) => {
   return (
     <section className='sidebar'>
         <nav className='flex flex-col gap-4'>
-            <Link href="/" className='mb-12 cursor-pointer items-center gap-2'>
+            <Link href="/" className='mb-12 cursor-pointer flex items-center gap-2'>
                 <Image
                 src='/icons/logo.svg'
                 width={34}
@@ -30,11 +29,27 @@ const Sidebar = ({user}: SiderbarProps) => {
                 <Link href={item.route} key={item.label}
                 className={cn('sidebar-link', {'bg-bank-gradient': isActive})}
                 >
-                  {item.label}
+                  <div className='relative size-6'>
+                    <Image
+                    src={item.imgURL}
+                    alt={item.label}
+                    fill
+                    className={cn({
+                      'brightness-[3] invert-0': isActive
+                    })}
+                    />
+                  </div>
+                  <p className={cn('sidebar-label', {'!text-white' : isActive})}>
+                    {item.label}
+                  </p>
                 </Link>
               )
             })}
+
+            USER
         </nav>
+
+          FOOTER
     </section>
   )
 }
