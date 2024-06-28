fa6b8c9af93e821bd6ff3dea3ffe17aaa7191313
Jay Calderon <cald0145@algonquinlive.com>
2024-06-28 00:19:55 -0400

    Add right sidebar with bank cards, add styling

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,29 +1,37 @@
 import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
-  const loggedIn = { firstName: 'Jay'};
+  const loggedIn = { firstName: "Jay", lastName: "Gatsby", email:'jay@jaycalderon.com' };
   return (
     <section className="home">
       <div className="home-content">
         <header className="home-header">
-          <HeaderBox 
-                      type="greeting"
-                      title="Welcome"
-                      user={loggedIn?.firstName || "Guest"}
-                      subtext="Access and manage your Financia account and transactions efficiently."
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your Financia account and transactions efficiently."
           />
 
-          <TotalBalanceBox 
-          accounts={[]}
-          totalBanks={1}
-          totalCurrentBalance={1250.35}
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
           />
         </header>
+        RECENT TRANSACTIONS
       </div>
+
+      <RightSidebar 
+      user={loggedIn} 
+      transactions={[]} 
+      banks={[{ currentBalance: 178.50}, { currentBalance: 800.40}]}
+      />
     </section>
-  )
+  );
 };
 
 export default Home;
