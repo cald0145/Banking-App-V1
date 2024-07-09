d2a9f182d6b270aca408b9e1a7d15ff48b59a593
Jay Calderon <cald0145@algonquinlive.com>
2024-07-09 18:47:10 -0400

    Refactor layout for proper redirect after log out, add account example image in sign up and sign in page

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,11 +1,23 @@
+import Image from "next/image";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <main>
-        {children}
+    <main className="flex min-h-scren w-full justify-between font-inter">
+      {children}
+      <div className="auth-asset">
+        <div>
+          <Image
+            src="/icons/auth-image.svg"
+            alt="Account Page Image"
+            width={500}
+            height={500}
+          />
+        </div>
+      </div>
     </main>
   );
 }
